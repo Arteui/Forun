69d3aede3e8d08ce087aa635c32daec8d0200e13
Arteui <156798534+Arteui@users.noreply.github.com>
2025-03-21 03:24:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ app.post('/comments', async (req, res) => {
 
     // Если есть parentId, это значит, что это ответ
     if (parentId) {
+      // Находим родительский комментарий по parentId
       const parent = await Comment.findById(parentId);
 
       if (parent) {
